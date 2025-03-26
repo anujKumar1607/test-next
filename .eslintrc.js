2c8cb31a47a91cf0b036b71bf41594c9fcbdd883
anuj srivastava <anuj7srivastava@gmail.com>
2025-03-26 12:59:10 +0530

    done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,36 @@
 /* eslint-env node */
-export const env = {
-  node: true,
-  browser: true,
-  es2022: true,
-};
-export const parserOptions = {
-  ecmaFeatures: {
-    jsx: true,
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2022: true,
   },
-  ecmaVersion: 'latest',
-  sourceType: 'module',
-};
-export const plugins = ['react', 'prettier'];
-// export const rules = {
-//   'react/react-in-jsx-scope': 'off',
-//   'react/jsx-uses-react': 'off',
-//   'react/prop-types': 'off',
-//   'no-unused-vars': 'warn', // Make unused vars warnings instead of errors
-// };
-export const settings = {
-  react: {
-    version: '18.2.0', // Explicitly set your React version (or 'detect')
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+  },
+  settings: {
+    react: {
+      version: '18.2.0',
+    },
+  },
+  ignorePatterns: ['.next/**'],
 };
-export const ignorePatterns = ['.next/**'];
